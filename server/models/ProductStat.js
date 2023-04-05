84f24a6d17a7f457dd81e8290226730381191f04
tomitose <tomitosello@gmail.com>
2023-04-05 16:45:00 -0300

    models - Product y ProductStat

---
+++ server/models/ProductStat.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const ProductStatSchema = new mongoose.Schema(
+    {
+        name: String,
+        price: Number,
+        description: String,
+        category: String,
+        rating: Number,
+        supply: Number,
+    },
+    {timestamps: true}
+)
+
+const ProductStat = mongoose.model("ProductStat", ProductStatSchema);
+
+export default ProductStat;
